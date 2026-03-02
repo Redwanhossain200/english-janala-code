416ebeb6ef461c2fa23a0edfdba47a6a379d3e5f
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-03-03 01:47:27 +0600

    update file

--- script/index.js
+++ script/index.js
@@ -13,13 +13,30 @@ const loadLevelWord = (id) => {
 
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
-  // wordContainer.innerHTML = "";
+  wordContainer.innerHTML = "";
+
+  // {
+  //   "id": 82,
+  //    "level": 1,
+  //    "word": "Car",
+  //     "meaning": "গাড়ি",
+  //     "pronunciation": "কার"
+  // }
 
   words.forEach(word => {
     console.log(word);
     const card = document.createElement("div");
     card.innerHTML = `
-    <p>Cat</p>
+    <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+      <h2 class="font-bold text-2xl">${word.word}</h2>
+      <p class="font-semibold">Meaning /Pronounciation</p>
+
+      <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+      <div class="flex justify-between items-center mt-12">
+        <button class="btn bg-[#1A91FF20] border-none hover:bg-[#1A91FF70]"><i class="fa-solid fa-circle-info"></i></button>
+        <button class="btn bg-[#1A91FF20] border-none hover:bg-[#1A91FF70]"><i class="fa-solid fa-volume-high"></i></button>
+      </div>
+    </div>
     `;
     wordContainer.append(card);
   })
