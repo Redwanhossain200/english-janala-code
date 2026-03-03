422983f178d60c3741519f5468eeb55c165861d6
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-03-03 15:51:15 +0600

    update file

--- script/index.js
+++ script/index.js
@@ -22,6 +22,53 @@ const loadLevelWord = (id) => {
     });
 }
 
+/* {
+  "word": "Diligent",
+    "meaning": "পরিশ্রমী",
+      "pronunciation": "ডিলিজেন্ট",
+        "level": 5,
+          "sentence": "He is a diligent student who studies every day.",
+            "points": 5,
+              "partsOfSpeech": "adjective",
+                "synonyms": [
+                  "hardworking",
+                  "industrious",
+                  "persistent"
+                ],
+                  "id": 4
+} */
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+}
+const displayWordDetails = (word) => {
+  console.log(word);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = `
+  <div class="">
+          <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i>:${word.pronunciation})</h2>
+        </div>
+        <div class="">
+          <h2 class="font-bold">Meaning</h2>
+          <p>${word.meaning}</p>
+        </div>
+        <div class="">
+          <h2 class="font-bold">Example</h2>
+          <p>${word.sentence}</p>
+        </div>
+        <div class="">
+          <h2 class="font-bold">Synonyms</h2>
+          <span class="btn">Syn1</span>
+          <span class="btn">Syn1</span>
+          <span class="btn">Syn1</span>
+        </div>
+  `;
+
+  document.getElementById("word_modal").showModal();
+}
+
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
@@ -56,7 +103,7 @@ const displayLevelWord = (words) => {
       } /  ${word.pronunciation ? word.pronunciation : "Pronounciation পাওয়া  যায়নি"
       }"</div>
       <div class="flex justify-between items-center mt-12">
-        <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF20] border-none hover:bg-[#1A91FF70]"><i class="fa-solid fa-circle-info"></i></button>
+        <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF20] border-none hover:bg-[#1A91FF70]"><i class="fa-solid fa-circle-info"></i></button>
         <button class="btn bg-[#1A91FF20] border-none hover:bg-[#1A91FF70]"><i class="fa-solid fa-volume-high"></i></button>
       </div>
     </div>
